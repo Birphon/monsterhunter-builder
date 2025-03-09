1b2d4189dd3b2a4b236af9ab2fa18157eed1e2d6
Birphon <61985658+Birphon@users.noreply.github.com>
2025-03-09 07:20:49 +0000

    Deploying to gh-pages from @ Birphon/monsterhunter-builder@0335fddb038882c925cb074477da046dceb22d2f 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // src/App.tsx
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import MonsterEncyclopedia from './pages/MonsterEncyclopedia';
+import Home from './pages/Home'; // Import the new Home page
+import MonsterEncyclopedia from './pages/MonsterEncyclopedia'; // Import the Monster Encyclopedia page
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MonsterEncyclopedia />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/monster-encyclopedia" element={<MonsterEncyclopedia />} />
       </Routes>
     </Router>
   );
